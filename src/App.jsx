ae9ce0dd8dbec6c8b13a29ac395b1faa567152d9
zohaib4321 <zohaib4321@gmail.com>
2024-03-13 16:10:38 +0500

    Project done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from "./components/Todos"
 
 function App() {
 
   return (
-    <>
-    </>
+    <div>
+    <h1 
+    className='text-2xl text-center mt-12 text-white/80'
+    >Manage Todos</h1>
+    <AddTodo />
+    <Todos />
+    </div>
   )
 }
 
--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -1,27 +1,22 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
-    todos: [
-        {
-            id: 1,
-            text: "Hello World!"
-        }
-    ]
+    todos: []
 }
 
 export const todoSlice = createSlice({
-    name: todo,
+    name: 'todo',
     initialState,
     reducers: {
         addTodo: (state, action) => {
             const todo = {
-                id: nanoid,
+                id: nanoid(),
                 text: action.payload
             }
             state.todos.push(todo)
         },
         removeTodo: (state, action) => {
-            state.todos = state.todos.filter((todo) => {todo.id !== action.payload})
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
         }
     }
 })
